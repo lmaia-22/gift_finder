ee5c968ddd374131d8093009aafa915b52f0c319
lmaia-22 <47107328+lmaia-22@users.noreply.github.com>
2021-09-08 15:19:03 +0100

    user corrections

---
+++ API/.history/api/models/user_20210908103333.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name:{ type: String, required:true },
+    username: {
+        type: String, 
+        lowercase: true, 
+        unique: true, 
+        required: [true, "can't be blank"], 
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'], 
+        index: true
+    },
+    email: {
+        type: String, 
+        required: true, 
+        useCreateIndex: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    country: {type: String, required:true },
+    password: { type: String, required:true },
+    role: { type: mongoose.Schema.Types.ObjectId, ref: 'Roles', required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
